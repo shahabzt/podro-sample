7061dc436071dded0626533be18ce3db8e6199ea
shahabzt <shahab.banijamali94@gmail.com>
2024-05-06 17:12:49 +0330

    feat: Add fonts and basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-
-import './App.css';
+import { RouterProvider } from "react-router-dom";
+import "./App.css";
+import AppRouter from "./pages/AppRouter";
 
 function App() {
   return (
-    <div className="App">
-    padro
-    </div>
+    <RouterProvider router={AppRouter} />
   );
 }
 
