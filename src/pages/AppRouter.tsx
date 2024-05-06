f8b0c9a7a59b5843d5b40a89365e3ee7a6fca658
shahabzt <shahab.banijamali94@gmail.com>
2024-05-06 17:12:17 +0330

    pkg: Install react-router package

---
+++ src/pages/AppRouter.tsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router-dom";
+
+
+const AppRouter = createBrowserRouter([
+    {
+      path: `/`,
+      element: (
+       <div>shahab</div>
+      ),
+    },
+    {
+      path: `/samira`,
+      element: (
+        <>samira</>
+      ),
+    },
+  ]);
+  
+  export default AppRouter;
