47a54c8fc902a86711a874cec117096e17124efe
shahabzt <shahab.banijamali94@gmail.com>
2024-05-07 22:09:24 +0330

    feat: Create a function for content in verification btn

---
+++ src/utils/functions/btnContent.ts
@@ -0,0 +1,11 @@
+export function btncontent(value: number, code:string) {
+    if (value === 0 && code !== "1111") {
+      return "ارسال مجدد";
+    } else if (code.length < 4 && code !== "1111") {
+      return value;
+    } else if (code === "1111") {
+      return "تایید";
+    } else {
+      return value;
+    }
+  }
