d1f60fd8591bff679e967948e0666a003670385d
shahabzt <shahab.banijamali94@gmail.com>
2024-05-07 15:08:03 +0330

    pkg: install and config redux and persist

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import AppRouter from "./pages/AppRouter";
+import { Layout } from "./styles/Layout";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "./store/store";
 
 function App() {
   return (
-    <RouterProvider router={AppRouter} />
+    <Layout>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+      <RouterProvider router={AppRouter} />
+      </PersistGate>
+      </Provider>
+    </Layout>
   );
 }
 
