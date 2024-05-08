ae3debe7cb0e2a428260f4a94a05788973c19d47
shahabzt <shahab.banijamali94@gmail.com>
2024-05-08 23:04:54 +0330

    fix: bugs fixed

--- src/styles/Layout.ts
+++ src/styles/Layout.ts
@@ -5,5 +5,5 @@ export const Layout = styled.div({
   display: "flex",
   justifyContent: "center",
   alignItems: "center",
-  height: "100vh",
+  minHeight:"100vh"
 });
--- src/utils/validations/registerPhoneNumber.ts
+++ src/utils/validations/registerPhoneNumber.ts
@@ -1,14 +1,12 @@
-import * as yup from 'yup';
-
-const phoneRegExp = /^(\+98|0)?9(1[0-9]|2[0-9]|3[0-9]|4[0-9]|90|91|20|21|22|23|30|31|32|33|34|35|36|37|38|39|90|91|92|93|94|99)\d{7}$/;
+import * as yup from "yup";
+import { iranPhoneNumberRegex } from "../regexes";
 
 const registrationFormScheme = yup.object().shape({
   phoneNumber: yup
     .string()
-    .typeError('ساختار شماره صحیح نمی باشد')
-    .matches(phoneRegExp, 'ساختار شماره صحیح نمی باشد')
-    .required('وارد کردن شماره موبایل اجباری است'),
-    
+    .typeError("ساختار شماره صحیح نمی باشد")
+    .matches(iranPhoneNumberRegex, "ساختار شماره صحیح نمی باشد")
+    .required("وارد کردن شماره موبایل اجباری است"),
 });
 
 export default registrationFormScheme;
